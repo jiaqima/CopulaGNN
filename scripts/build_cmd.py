390b55fcaa19f28ad2c00a6e973dee80408dcf3b
jiaqima-mac <mjq44bruce@163.com>
2020-03-22 16:54:57 -0400

    add non-grid-search build_cmd.py in comments

--- scripts/build_cmd.py
+++ scripts/build_cmd.py
@@ -16,17 +16,21 @@ def build_cmd(cmd):
             build_cmd(cmd + " --model_type {}".format(model_type))
     elif "tau" not in cmd:
         for tau in [1.]:
+        # for tau in [0.1, 1., 5.]:
             if "cgcn" in cmd or "cmlp" in cmd:
                 for m_tau in [tau / 10, tau / 2, tau, tau * 2, tau * 10]:
+                # for m_tau in [tau]:
                     build_cmd(cmd + " --tau {} --m_tau {}".format(tau, m_tau))
             else:
                 build_cmd(cmd + " --tau {}".format(tau))
     elif "gamma" not in cmd:
         for gamma in [0.5]:
+        # for gamma in [0.1]:
             if "cgcn" in cmd or "cmlp" in cmd:
                 for m_gamma in [
                         gamma / 5, gamma / 2, gamma, gamma * 2, gamma * 5
                 ]:
+                # for m_gamma in [gamma]:
                     build_cmd(
                         cmd +
                         " --gamma {} --m_gamma {}".format(gamma, m_gamma))
